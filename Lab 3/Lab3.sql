3fcfc3594c18b12e49f11d9e90c5117c87f3fcc6
Sergey <50848091+I-SER-I@users.noreply.github.com>
2020-05-01 17:31:05 +0300

    Update Lab3.sql

--- Lab 3/Lab3.sql
+++ Lab 3/Lab3.sql
@@ -128,3 +128,41 @@ GROUP BY
 	P.Name
 ORDER BY
 	COUNT(*) DESC
+
+/* 10 */
+SELECT TOP 1
+    PC.Name
+FROM
+    Production.Product AS P
+JOIN
+    Production.ProductSubcategory AS PSC
+ON
+    P.ProductSubcategoryID = PSC.ProductSubcategoryID
+JOIN
+    Production.ProductCategory AS PC
+ON
+    PSC.ProductCategoryID = PC.ProductCategoryID
+JOIN
+    Purchasing.ProductVendor AS PPV
+ON
+    P.ProductID = PPV.ProductID
+GROUP BY
+    PC.Name
+ORDER BY
+    COUNT(*) DESC
+    
+/* 11 */
+SELECT
+    PC.Name, COUNT(DISTINCT P.ProductSubcategoryID), COUNT(ProductID)
+FROM
+    Production.Product AS P
+JOIN
+    Production.ProductSubcategory AS PSC
+ON
+    P.ProductSubcategoryID = PSC.ProductSubcategoryID
+JOIN
+    Production.ProductCategory AS PC
+ON
+    PC.ProductCategoryID = PSC.ProductCategoryID
+GROUP BY
+    PC.Name
