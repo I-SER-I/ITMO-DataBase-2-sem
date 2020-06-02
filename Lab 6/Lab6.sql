2bf45139a3edd43f099f91cbfba328ae38c31f39
Sergey <50848091+I-SER-I@users.noreply.github.com>
2020-06-02 19:28:19 +0300

    Add files via upload

---
+++ Lab 6/Lab6.sql
@@ -0,0 +1,37 @@
+/* 1 */
+SELECT 
+	Name, SalesOrderID, (SUM(OrderQty) OVER(PARTITION BY Name, SalesOrderID) * UnitPrice)
+FROM 
+	Sales.SalesOrderDetail AS SOD 
+JOIN 
+	Production.Product AS PP 
+ON 
+	SOD.ProductID = PP.ProductID
+
+/* 2 */
+SELECT 
+	Name, StandardCost, (StandardCost - MIN(StandardCost) OVER(PARTITION BY ProductSubcategoryID)) AS Diff
+FROM 
+	Production.Product
+
+/* 3 */
+SELECT 
+	CustomerID, SalesOrderID, (ROW_NUMBER() OVER(PARTITION BY CustomerID ORDER BY OrderDate)) AS RowNum
+FROM 
+	Sales.SalesOrderHeader
+
+/* 4 */
+WITH 
+	ProductsAndAvg (ProductID, StandardCost, ProductSubcategoryID, AvgCostSubcategory) AS
+	(
+		SELECT 
+			ProductID, StandardCost, ProductSubcategoryID, (AVG(StandardCost) OVER(PARTITION BY ProductSubcategoryID)) 
+		FROM 
+			Production.Product
+	)
+SELECT 
+	ProductID 
+FROM 
+	ProductsAndAvg
+WHERE 
+	StandardCost > AvgCostSubcategory AND ProductSubcategoryID IS NOT NULL
